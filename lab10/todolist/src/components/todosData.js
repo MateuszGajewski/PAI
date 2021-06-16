bf5e8aa715a59bd692008569b88cac4a658d2b25
arabica <mg96272@gmail.com>
2021-06-16 18:25:07 +0200

    Zad

---
+++ lab10/todolist/src/components/todosData.js
@@ -0,0 +1,16 @@
+
+let todosData = [
+    {
+        id: 1,
+        text: "Do homework",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Excersise",
+        completed: true
+    }
+]
+
+export default todosData
+    
